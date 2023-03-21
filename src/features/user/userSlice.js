1157ff0a085b0ac244495e8dd4de15b9465ea56d
quan <nguyenquan07112001@gmail.com>
2023-03-21 16:40:45 +0700

    handle register api

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+import { authService } from "./userService";
+
+const getUserfromlocalStorage = localStorage.getItem("user")
+  ? JSON.parse(localStorage.getItem("user"))
+  : null;
+
+export const registerUser = createAsyncThunk(
+  "auth/register",
+  async (userData, thunkAPI) => {
+    try {
+      return await authService.register(userData);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+const initialState = {
+  user: "",
+  isError: false,
+  isLoading: false,
+  isSuccess: false,
+  message: "",
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: initialState,
+  reducers: {},
+  extraReducers: (buider) => {
+    buider
+      .addCase(registerUser.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(registerUser.fulfilled, (state, action) => {
+        state.isError = false;
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.creatUser = action.payload;
+        state.message = "success";
+        if (state.isSuccess === true) {
+          toast.info("User Created Successfully");
+        }
+      })
+      .addCase(registerUser.rejected, (state, action) => {
+        state.isError = true;
+        state.isSuccess = false;
+        state.message = action.error;
+        state.isLoading = false;
+        if (state.isError === true) {
+          toast.info(action.error);
+        }
+      });
+  },
+});
+
+export default authSlice.reducer;
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -4,7 +4,35 @@ import BreadCrumb from "../components/BreadCrumb";
 import { Link } from "react-router-dom";
 import Container from "../components/Container";
 import CustomInput from "../components/CustomInput";
+import { useFormik } from "formik";
+import { useDispatch, useSelector } from "react-redux";
+import { registerUser } from "../features/user/userSlice";
+import * as yup from "yup";
+
+const signUpSchema = yup.object({
+  firstname: yup.string().required("Fisrt Name is Required"),
+  lastname: yup.string().required("Last Name is Required"),
+  email: yup.string().nullable().email("Email should be valid"),
+  mobile: yup.string().required("Mobile No is Required"),
+  password: yup.string().required("Password is Required"),
+});
+
 const Signup = () => {
+  const dispath = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      firstname: "",
+      lastname: "",
+      email: "",
+      mobile: "",
+      password: "",
+    },
+    validationSchema: signUpSchema,
+    onSubmit: (values) => {
+      dispath(registerUser(values));
+    },
+  });
+
   return (
     <>
       <Meta title={"Sign Up"} />
@@ -14,19 +42,61 @@ const Signup = () => {
           <div className="col-12 ">
             <div className="auth-card">
               <h3 className="text-center mb-3 ">Sign Up</h3>
-              <form action="" className="d-flex flex-column gap-15">
-                <CustomInput type="text" name="name" placeholder="Name" />
-                <CustomInput type="email" name="email" placeholder="Email" />
+              <form
+                action=""
+                onSubmit={formik.handleSubmit}
+                className="d-flex flex-column gap-15">
+                <CustomInput
+                  type="text"
+                  name="firstname"
+                  placeholder="First name"
+                  value={formik.values.firstname}
+                  onChange={formik.handleChange("firstname")}
+                  onBlur={formik.handleBlur("firstname")}
+                />
+                <div className="error">
+                  {formik.touched.firstname && formik.errors.firstname}
+                </div>
+                <CustomInput
+                  type="text"
+                  name="lastname"
+                  placeholder="Last name"
+                  onChange={formik.handleChange("lastname")}
+                  onBlur={formik.handleBlur("lastname")}
+                />
+                <div className="error">
+                  {formik.touched.lastname && formik.errors.lastname}
+                </div>
+                <CustomInput
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  onChange={formik.handleChange("email")}
+                  onBlur={formik.handleBlur("email")}
+                />
+                <div className="error">
+                  {formik.touched.email && formik.errors.email}
+                </div>
                 <CustomInput
                   type="tel"
                   name="mobile"
                   placeholder="Mobile Number"
+                  onChange={formik.handleChange("mobile")}
+                  onBlur={formik.handleBlur("mobile")}
                 />
+                <div className="error">
+                  {formik.touched.mobile && formik.errors.mobile}
+                </div>
                 <CustomInput
                   type="password"
                   name="password"
                   placeholder="Password"
+                  onChange={formik.handleChange("password")}
+                  onBlur={formik.handleBlur("password")}
                 />
+                <div className="error">
+                  {formik.touched.password && formik.errors.password}
+                </div>
                 <div>
                   <div className=" mt-3 d-flex justify-content-center align-items-center gap-15">
                     <button className="button border-0" type="submit">
