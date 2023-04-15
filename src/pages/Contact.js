53b84551870069ab7d7aef13b0aa33659ab2a8fe
quan <nguyenquan07112001@gmail.com>
2023-04-15 17:04:15 +0700

    contact form submit

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,12 +4,67 @@ import Meta from "../components/Meta";
 import { AiFillHome, AiFillPhone, AiTwotoneMail } from "react-icons/ai";
 import { FaInfo } from "react-icons/fa";
 import Container from "../components/Container";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { createQuery } from "../features/contact/contactSlice";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import { ToastContainer } from "react-toastify";
+
+const contactSchema = yup.object({
+  name: yup.string().required("Name is Required"),
+  email: yup
+    .string()
+    .nullable()
+    .email("Email Should be valid")
+    .required("Email is Required"),
+  mobile: yup
+    .string()
+    .default("")
+    .nullable()
+    .required("Mobile Number is Required"),
+  comment: yup.string().default("").nullable().required("Comment is Required"),
+});
 const Contact = () => {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      comment: "",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values) => {
+      dispatch(
+        createQuery({
+          name: values.name,
+          email: values.email,
+          mobile: values.mobile,
+          comment: values.comment,
+        })
+      );
+    },
+  });
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+      ;
       <Meta title={"Contact Us"} />
       <BreadCrumb title="Contact Us" />
-
       <Container class1="contact-wrapper py-5 home-wrapper-2">
         <div className="row set-padding ">
           <div className="col-12">
@@ -27,65 +82,95 @@ const Contact = () => {
             <div className="contact-inner-wrapper d-flex justify-content-between">
               <div>
                 <h3 className="contact-title mb-4"> Contact </h3>
-                <form action="" className="d-flex flex-column gap-15">
+                <form
+                  action=""
+                  onSubmit={formik.handleSubmit}
+                  className="d-flex flex-column gap-15">
                   <div>
                     <input
                       type="text"
                       className="form-control"
                       placeholder="Name"
+                      name="name"
+                      onChange={formik.handleChange("name")}
+                      onBlur={formik.handleBlur("name")}
+                      value={formik.values.name}
                     />
+                    <div className="errors">
+                      {formik.touched.name && formik.errors.name}
+                    </div>
                   </div>
                   <div>
                     <input
-                      type="text"
+                      type="email"
                       className="form-control"
                       placeholder="Email"
+                      name="email"
+                      onChange={formik.handleChange("email")}
+                      onBlur={formik.handleBlur("email")}
+                      value={formik.values.email}
                     />
+                    <div className="errors">
+                      {formik.touched.email && formik.errors.email}
+                    </div>
                   </div>
                   <div>
                     <input
-                      type="text"
+                      type="tel"
                       className="form-control"
-                      placeholder="Phone Number"
+                      placeholder="Mobile Number"
+                      name="mobile"
+                      onChange={formik.handleChange("mobile")}
+                      onBlur={formik.handleBlur("mobile")}
+                      value={formik.values.mobile}
                     />
+                    <div className="errors">
+                      {formik.touched.mobile && formik.errors.mobile}
+                    </div>
                   </div>
                   <div>
                     <textarea
-                      name=""
-                      id=""
                       className="w-100 form-control"
+                      id=""
                       cols="30"
                       rows="4"
-                      placeholder="Comment"></textarea>
+                      placeholder="Comment"
+                      name="comment"
+                      onChange={formik.handleChange("comment")}
+                      onBlur={formik.handleBlur("comment")}
+                      value={formik.values.comment}></textarea>
+                    <div className="errors">
+                      {formik.touched.comment && formik.errors.comment}
+                    </div>
                   </div>
                   <div>
-                    <button className="button border-0">Send</button>
+                    <button className="button border-0">Submit</button>
                   </div>
                 </form>
               </div>
               <div>
-                <h3 className="contact-title mb-4"> Get In Touch With Us </h3>
+                <h3 className="contact-title mb-4">Get in touch with us</h3>
                 <div>
                   <ul className="ps-0">
                     <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiFillHome className="fs-5" />
+                      <AiOutlineHome className="fs-5" />
                       <address className="mb-0">
                         Hno:3/2, Xuan Khanh, Ninh Kieu, Can Tho
                       </address>
                     </li>
-                    <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiFillPhone className="fs-5" />
+                    <li className="mb-3  d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
                       <a href="tel:+83 345384130">+83 345384130</a>
                     </li>
                     <li className="mb-3 d-flex gap-15 align-items-center">
-                      <AiTwotoneMail className="fs-5" />
+                      <AiOutlineMail className="fs-5" />
                       <a href="mailto:quanb1910130@student.ctu.edu.vn">
                         mailto:quanb1910130@student.ctu.edu.vn
                       </a>
                     </li>
                     <li className="mb-3 d-flex gap-15 align-items-center">
-                      <FaInfo className="fs-5" />
-                      <p> Monday – Friday 10 AM – 8 PM</p>
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Monday – Friday 10 AM – 8 PM</p>
                     </li>
                   </ul>
                 </div>
